c20fa6653d2432253940017ea253e8455d87f70e
dbehdghks <rdwhan@naver.com>
2025-10-17 21:44:06 +0900

    project commit

---
+++ src/main/java/com/example/soldesk_backend/SoldeskBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.soldesk_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoldeskBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoldeskBackendApplication.class, args);
+	}
+
+}
